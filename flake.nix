dfce1e911a562c250ba5e7070d0b526eba6ea299
Lucian Ursu <lucian.ursu@gmail.com>
2023-10-03 15:23:06 +0300

    Create repo

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "UI Playground";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, treefmt-nix }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs.elmPackages; [
+              elm
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        treefmt-nix.lib.mkWrapper
+          pkgs
+          {
+            projectRootFile = "flake.nix";
+            programs.nixpkgs-fmt.enable = true;
+            programs.elm-format.enable = true;
+          }
+      );
+    };
+}
